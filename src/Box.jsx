d041adf278162bea92f52385bce6128361cbb542
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2024-08-23 23:33:42 +0300

    Using props

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,8 +1,26 @@
-export default function Box() {
+export default function Box(props) {
   return (
-    <mesh>
+    <mesh {...props}>
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe />
     </mesh>
   );
 }
+
+// export default function Box(props) {
+//   return (
+//     <mesh position={props.position} name={props.name}>
+//       <boxGeometry />
+//       <meshBasicMaterial color={0x00ff00} wireframe={props.wireframe} />
+//     </mesh>
+//   );
+// }
+
+// export default function Box({ position, name, wireframe }) {
+//   return (
+//     <mesh position={position} name={name}>
+//       <boxGeometry />
+//       <meshBasicMaterial color={0x00ff00} wireframe={wireframe} />
+//     </mesh>
+//   );
+// }
