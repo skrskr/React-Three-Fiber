2d29f130fad48c38ecc8890e19677f4e765a38f0
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2024-08-24 15:45:51 +0300

    add enviroment map

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Stats, OrbitControls, Circle } from '@react-three/drei'
+import { Stats, OrbitControls, Circle, Environment } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
 
@@ -7,6 +7,7 @@ export default function App() {
 
   return (
     <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+      <Environment preset="forest" background blur={0.5}/>
       <directionalLight
         position={[3.3, 1.0, 4.4]}
         castShadow
