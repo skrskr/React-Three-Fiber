914bc395080a4013d0612e2643dcb2d38e2214ce
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2024-08-24 13:49:28 +0300

    Add materials

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,36 @@
-import { Canvas } from "@react-three/fiber";
-
-import * as THREE from "three";
-import Polyhedron from "./Polyhedron";
-import { OrbitControls, Stats } from "@react-three/drei";
-import { Leva, useControls } from "leva";
-import { useMemo } from "react";
+import { Canvas } from '@react-three/fiber'
+import Polyhedron from './Polyhedron'
+import * as THREE from 'three'
+import { Stats, OrbitControls } from '@react-three/drei'
 
 export default function App() {
-  const polyhedron = [
-    new THREE.BoxGeometry(),
-    new THREE.SphereGeometry(0.78),
-    new THREE.DodecahedronGeometry(0.78),
-  ];
-
-  // const color = useControls({
-  //   value: "#000000",
-  // });
-
-  const options = useMemo(() => {
-    return {
-      x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      visible: true,
-      color: { value: 'lime' },
-    };
-  }, []);
-
-  const pA = useControls("Polyhedron A", options);
-  const pB = useControls("Polyhedron B", options);
-
   return (
-    <>
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      {/* <color attach={"background"} args={[color.value]} /> */}
-      <Polyhedron 
-        position={[-1, 1, 0]} 
-        rotation={[pA.x, pA.y, pA.z]}
-        visible={pA.visible}
-        color={pA.color}
-        polyhedron={polyhedron} 
+    <Canvas camera={{ position: [-1, 4, 2.5] }}>
+      <directionalLight position={[1, 1, 1]} />
+      <Polyhedron
+        name="meshBasicMaterial"
+        position={[-3, 1, 0]}
+        material={new THREE.MeshBasicMaterial()}
       />
-      <Polyhedron 
-        position={[1, 1, 0]} 
-        rotation={[pB.x, pB.y, pB.z]}
-        visible={pB.visible}
-        color={pB.color}
-        polyhedron={polyhedron} 
+      <Polyhedron
+        name="meshNormalMaterial"
+        position={[-1, 1, 0]}
+        material={new THREE.MeshNormalMaterial()}
       />
-     
-      <OrbitControls />
-      <axesHelper args={[5]}/>
-      <gridHelper 
-        args={[20, 20, 0xff0000, 'teal']}
-        // rotation-x={Math.PI / 4}
-        // rotation-y={Math.PI / 4}
+      <Polyhedron
+        name="meshPhongMaterial"
+        position={[1, 1, 0]}
+        material={new THREE.MeshPhongMaterial()}
       />
+      <Polyhedron
+        name="meshStandardMaterial"
+        position={[3, 1, 0]}
+        material={new THREE.MeshStandardMaterial()}
+      />
+      <OrbitControls target-y={1} />
+      <axesHelper args={[5]} />
+      <gridHelper />
       <Stats />
-
     </Canvas>
-      <Leva collapsed />
-    </>
-  );
+  )
 }
--- src/Polyhedron.jsx
+++ src/Polyhedron.jsx
@@ -1,31 +1,41 @@
-import { useFrame } from "@react-three/fiber";
-import { useEffect, useRef, useState } from "react";
+import { useRef } from 'react'
+import { useControls } from 'leva'
+import * as THREE from 'three'
+import { useFrame } from '@react-three/fiber'
 
-export default function Polyhedron({polyhedron, color, ...props}) {
-  const ref = useRef();
-
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log(ref.current.geometry.uuid)
-  });
-
+export default function Polyhedron(props) {
+  const ref = useRef()
 
   useFrame((_, delta) => {
-      ref.current.rotation.x += delta;
-      ref.current.rotation.y += delta * 0.5;
-  });
+    ref.current.rotation.x += 0.2 * delta
+    ref.current.rotation.y += 0.05 * delta
+  })
+
+  useControls(props.name, {
+    wireframe: {
+      value: false,
+      onChange: (v) => {
+        ref.current.material.wireframe = v
+      },
+    },
+    flatShading: {
+      value: true,
+      onChange: (v) => {
+        ref.current.material.flatShading = v
+        ref.current.material.needsUpdate = true
+      },
+    },
+    color: {
+      value: 'lime',
+      onChange: (v) => {
+        ref.current.material.color = new THREE.Color(v)
+      },
+    },
+  })
 
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      onPointerDown={() => setCount((count + 1) % 2)}
-      geometry={polyhedron[count]}
-    >
-      <meshBasicMaterial color={ color } wireframe />
-
-      {/* <axesHelper /> */}
+    <mesh {...props} ref={ref}>
+      <icosahedronGeometry args={[1, 1]} />
     </mesh>
-  );
+  )
 }
